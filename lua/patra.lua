b8885e084e43904a7a949cff2203c9a15ea36d0b
Bhanuka <bhanukamax@gmail.com>
2023-04-15 03:00:08 +0530

    feat: properly create theme config and pass as cli arg

--- lua/patra.lua
+++ lua/patra.lua
@@ -8,6 +8,9 @@ local function write_to_file(file_path, contents)
   local file = io.open(file_path, 'w')
   if file then
     file:write(contents)
+    file:close()
+  else
+    vim.print("Error: Could not open file " .. file_path)
   end
 end
 
@@ -24,7 +27,7 @@ end
 local function on_exit(job_id, code, event)
   vim.wo.rnu = true
   vim.wo.nu = true
-  vim.pretty_print({ job_id, code, event })
+  vim.print({ job_id, code, event })
   local contents = get_file_contents(vim.g.patra_temp_file)
   -- vim.api.nvim_buf_delete(buffer, {})
   buffer = -1
@@ -50,22 +53,21 @@ end
 local function get_hl(hl_name, fg_bg)
   local color = vim.api.nvim_get_hl_by_name(hl_name, true)[fg_bg]
   local bit = require("bit")
+  if color == nil then
+    vim.print({
+        "NIL",
+        hl_name,
+        fg_bg,
+        color
+    })
+    return ""
+  end
   local r = bit.band(bit.rshift(color, 16), 0xff)
   local g = bit.band(bit.rshift(color, 8), 0xff)
   local b = bit.band(color, 0xff)
 
   local cursor_color = string.format("#%02x%02x%02x", r, g, b)
   return cursor_color
-end
-
-local function get_theme()
-  local cursor_color = get_hl("CursorLine", "background")
-  local normal       = get_hl("Normal", "foreground")
-  local theme        = ""
-  theme              = theme .. "--theme-file-focus-bg=" .. '"' .. cursor_color .. '"'
-  theme              = theme .. " --file-fg=" .. '"' .. normal .. '"'
-  -- TODO: get and pass Directory color
-  return theme
 end
 
 local function open_patra()
@@ -75,31 +77,59 @@ local function open_patra()
   vim.cmd("enew")
   vim.g.patra_temp_file = vim.fn.tempname()
 
-  local cmd = 'patra ' .. dir_path .. ' --selection-path ' .. vim.g.patra_temp_file .. ' ' .. get_theme()
+  local cmd = 'patra ' .. dir_path .. ' --selection-path ' .. vim.g.patra_temp_file
+  if vim.g.patra_config ~= nil then
+    cmd = cmd .. ' --config ' .. vim.g.patra_config
+  end
   vim.wo.rnu = false
   vim.wo.nu = false
 
   exec_patra_cmd(cmd)
 end
 
+local function theme_update_content(theme, key, value)
+  if theme == "" then
+    theme = '[theme]\n'
+    theme = theme .. key .. '=' .. "'" .. value .. "'\n"
+  else
+    theme = theme .. key .. '=' .. "'" .. value .. "'\n"
+  end
+  return theme
+end
+
 local function setup_theme()
-  local theme = [[
-[theme]
-file_fg = '#ff0000'
-file_focus_bg = '#2a2b3c'
-dir_fg = '#228900'
-  ]]
+  -- local dir          = get_hl("Number", "foreground")
+  local dir          = get_hl("Directory", "foreground")
+  local normal       = get_hl("Normal", "foreground")
+  -- local dir          = get_hl("Directory", "foreground")
+  local cursor_color = get_hl("CursorLine", "background")
+  -- TODO: get and pass Directory color
+
+
+  vim.print({ corsor = cursor_color, normal = normal, dir = dir })
+  local theme = ''
+
+  if normal ~= "" then
+    theme = theme_update_content(theme, 'file_fg', normal)
+  end
+  if cursor_color ~= "" then
+    theme = theme_update_content(theme, 'file_focus_bg', cursor_color)
+  end
+  if dir ~= "" then
+    theme = theme_update_content(theme, 'dir_fg', dir)
+  end
   vim.g.patra_config = vim.fn.tempname()
   local file = vim.g.patra_config
+  vim.print({ the = 'the theme', file, theme })
   write_to_file(file, theme)
-
-  print(theme)
 end
 local function setup()
   setup_theme()
 end
 
+
 return {
-  open_patra = open_patra,
-  setup = setup
+    open_patra = open_patra,
+    setup_theme = setup_theme,
+    setup = setup,
 }
--- plugin/patra.vim
+++ plugin/patra.vim
@@ -1,2 +1,4 @@
 
 command! Patra :lua require"patra".open_patra()
+command! PatraTheme :lua require"patra".setup_theme()
+autocmd ColorScheme * PatraTheme
